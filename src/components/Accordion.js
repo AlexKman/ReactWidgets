f3a82ba73ed8f9686427716b170d3c405c3b5b40
Desktopy12 <Keeley.lp@gmail.com>
2021-04-27 19:31:30 +0100

    Added logic to change css active class when accordion item is active or not

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Accordion=({items})=>{
-const renderedItems = items.map(item=>{
-    return <div key={item.title}>
-        <div className="title active">
+    const [activeIndex,setActiveIndex] = useState(null);
+
+const ontitleClick=(index)=>{
+setActiveIndex(index);
+}
+
+const renderedItems = items.map((item,index)=>{
+    const active=index===activeIndex?'active':'';
+    return <React.Fragment key={item.title}>
+        <div onClick={()=>{ontitleClick(index)}}  className={`title + ${active}`}>
             <i className="dropdown icon"></i>
             {item.title}
         </div>
-        <div className="content active">
+        <div className={`content + ${active}`}>
 <p>{item.content}</p>
         </div>
-    </div>
+        </React.Fragment>
 })
 
 return <div className="ui styled accordion">
     {renderedItems}
+    <h1>{activeIndex}</h1>
 </div>
 }
 
