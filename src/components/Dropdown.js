b63ece90f7e8642a5b1981b16b1d204e1000a1d2
Desktopy12 <Keeley.lp@gmail.com>
2021-05-02 15:40:23 +0100

    Application has been updated, using the dropdown component we are now sending text to the google translate api and receiving the text back in the specific translated language

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 
-const Dropdown = ({ options, selected, onSelected }) => {
+const Dropdown = ({ options, selected, onSelected, label }) => {
   const [open, setOpen] = useState(false);
   const ref = useRef();
 
@@ -49,7 +49,7 @@ const Dropdown = ({ options, selected, onSelected }) => {
   return (
     <div ref={ref} className="ui form">
       <div className="field">
-        <label className="label">Select a Color</label>
+        <label className="label">{label}</label>
         <div
           onClick={() => setOpen(!open)}
           className={`ui selection dropdown ${open ? "visible active" : ""}`}
