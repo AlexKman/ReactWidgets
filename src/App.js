c98af13a98c128162818db538582a3a8bb4debc6
Desktopy12 <Keeley.lp@gmail.com>
2021-05-02 15:06:59 +0100

    Project has been updated to a different 'widget', will be changed again so just keeping this repo as a reference for myself

--- src/App.js
+++ src/App.js
@@ -1,27 +1,53 @@
-import React from 'react';
-import Accordion from './components/Accordion';
-import Search from './components/Search';
+import React, { useState } from "react";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
-const items=[
-    {
-        title:'What is React?',
-        content:'React is a front end JavaScript framework'
-    },
-    {
-        title:"Why use React?",
-        content:"React is an epic js library"
-    },
-    {
-        title:"Why is it epic?",
-        content:"Components and dat"
-    }
-]
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a front end JavaScript framework",
+  },
+  {
+    title: "Why use React?",
+    content: "React is an epic js library",
+  },
+  {
+    title: "Why is it epic?",
+    content: "Components and dat",
+  },
+];
 
-export default()=>{
-    return (
+const options = [
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+  {
+    label: "The Color Green",
+    value: "green",
+  },
+  {
+    label: "The Color Blue",
+    value: "blue",
+  },
+];
+
+export default () => {
+  const [selected, setSelected] = useState(options[0]);
+  const [showDropdown, setShowDropdown] = useState(true);
+  return (
     <div>
-        {/* <Accordion items={items}></Accordion> */}
-        <Search></Search>
+      <button onClick={() => setShowDropdown(!showDropdown)}>
+        Toggle Dropdown
+      </button>
+      {showDropdown ? (
+        <Dropdown
+          selected={selected}
+          onSelected={setSelected}
+          options={options}
+        ></Dropdown>
+      ) : null}
     </div>
-    )
-}
+  );
+};
